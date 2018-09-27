002c03ca8e49ef112fdc8bd8f6583fb584807474
Joel Cox <joel.cox.dev@gmail.com>
2018-09-27 18:59:25 -0500

    initial commit

---
+++ exhibitors.js
@@ -0,0 +1,344 @@
+const exhibitors = [
+    {
+        name: 'All Weather Inc',
+        img: 'http://www.atc-network.com/Upload/Industry/33698/AWIlogo200.jpg',
+        site: 'http://www.allweatherinc.com/',
+        level: 'exhibitor'
+    },
+    {
+        name: 'American Benefits Exchange',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/7368147004b546f6b0941202f8c862f6.jpg',
+        site: 'http://thinkabx.com/',
+        level: 'bronze'
+    },
+    {
+        name: 'ATAC',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/e7940caa1b66472485c0b1a1f63cb91b.jpg',
+        site: 'http://www.atacusa.com/',
+        level: 'bronze'
+    },
+    {
+        name: 'ATO FAA COMM',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/37b1739ea9a24bba9d59e2653ba0d9cc.png',
+        site: 'https://www.faa.gov/about/office_org/headquarters_offices/acr/',
+        level: 'exhibitor'
+    },
+    {
+        name: 'Blue Cross Blue Shield FEP',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/cc4a1ee937e344c19e63cd3e155cf3ef.jpg',
+        site: 'http://www.bcbs.com/',
+        level: 'gold'
+    },
+    {
+        name: 'cobec',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/273d823bbcb6456cb9b65b23f9578f69.png',
+        site: 'http://cobec.com/',
+        level: 'exhibitor'
+    },
+    {
+        name: 'Comtech EFD Memotec',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/cf8aa17f24c241d587f19b6babbf09d0.jpg',
+        site: 'http://www.comtechefdata.com',
+        level: 'exhibitor'
+    },
+    {
+        name: 'cavansolutions',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/3f5e56b69baa4ef6bb9a6407425977b9.png',
+        site: 'http://cavansolutions.com/',
+        level: 'exhibitor'
+    },
+    {
+        name: 'CGH Technologies',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/3a845916717643cba8d88f864b562e9e.jpg',
+        site: 'http://www.cghtech.com/',
+        level: 'exhibitor'
+    },
+    {
+        name: 'Concept Seating',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/d4c7679139f849a1a7b9a33401022d1d.jpg',
+        site: 'http://www.conceptseating.com/',
+        level: 'exhibitor'
+    },
+    {
+        name: 'Crown Consulting',
+        img: 'http://cignus.aero/wp-content/uploads/2017/02/CCI_Shadow.png',
+        site: 'https://www.crownci.com/',
+        level: 'bronze'
+    },
+    {
+        name: 'EASAT',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/3f826413ec8f49fea4edb8eef93c5c0f.png',
+        site: 'http://www.easat.com/',
+        level: 'exhibitor'
+    },
+    {
+        name: 'Eizo',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/71d3c0efb5ca404585964b3c026d6493.jpg',
+        site: 'http://www.eizo.com/home/',
+        level: 'bronze',
+    },
+    {
+        name: 'Esterline Federal',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/0086041b6e6347b0a783b07938ae0ad5.png',
+        site: 'https://www.esterline.com/',
+        level: 'exhibitor'
+    },
+    {
+        name: 'Evans Consoles',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/72896b0b7f584efd9633df1633748441.jpg',
+        site: 'http://www.evansonline.com/',
+        level: 'silver'
+    },
+    {
+        name: 'FAA Office of Civil Rights',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/45fbc7854c114eddb934eb3b37352ddd.png',
+        site: 'https://www.faa.gov/about/office_org/headquarters_offices/acr/',
+        level: 'exhibitor'
+    },
+    {
+        name: 'FAAMA',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/fce1302f4ecf451f8756943d696ddeca.jpg',
+        site: 'http://www.faama.org/',
+        level: 'exhibitor'
+    },
+    {
+        name: 'FEDS',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/1fa501110efb48f486a6c19ddc38de0f.jpg',
+        site: 'http://www.fedsprotection.com/',
+        level: 'exhibitor'
+    },
+    {
+        name: 'Frequentis',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/53264dc8b3ca436e9786f9d4a5ab7a01.jpg',
+        site: 'http://www.frequentis.com/en/us/home/',
+        level: 'bronze'
+    },
+    {
+        name: 'First Command',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/129b11a49927453e9d8d88bf09de0135.png',
+        site: 'http://www.firstcommand.com/',
+        level: 'exhibitor'
+    },
+    {
+        name: 'Geico',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/0fc79b3dce1343d78a96b832fc330296.jpg',
+        site: 'https://www.geico.com/landingpage/member-discount/?logo=70411',
+        level: 'platinum'
+    },
+    {
+        name: 'General Dynamics Information Technology',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/bb5603d162fe4580bc811110ad308fc4.jpg',
+        site: 'http://www.gdit.com/',
+        level: 'silver'
+    },
+    {
+        name: 'GPIS Employee Benefit Specialist',
+        img: 'https://gpis4u.org/wp-content/uploads/2018/06/GPISLogo.png',//??
+        site: 'https://gpis4u.org/',
+        level: 'exhibitor'
+    },
+    {
+        name: 'Hourglass Travel',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/412ec71f52e8403e906fdab141d270f1.png',
+        site: 'http://hourglasstravel.com/',
+        level: 'bronze'
+    },
+    {
+        name: 'ISSEusa, LLC',
+        img: 'http://www.ihseusa.com/images/logo.png',
+        site: 'http://www.ihseusa.com/',
+        level: 'exhibitor'
+    },
+    {
+        name: 'Inifina',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/6038e007908b47618dce40010f7e4455.png',
+        site: 'http://www.infina.net/',
+        level: 'silver'
+    },
+    {
+        name: 'jlansolutions',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/4c32d56fb5674c008b3b7704ab5928c0.png',
+        site: 'http://www.jlansolutions.com/',
+        level: 'exhibitor' 
+    },
+    {
+        name: 'JMA Solutions',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/63dbfe67586e463d901f7ad608f4f00b.jpg',
+        site: 'http://www.jma-solutions.com/',
+        level: 'bronze'
+    },
+    {
+        name: 'kaiser permanente',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/5af24500d9e4437080f34a6191eb0491.jpg',
+        site: 'https://healthplans.kaiserpermanente.org/federalemployees/',
+        level: 'exhibitor'
+    },
+    {
+        name: 'leidos',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/adafae8689704039bb531d9a39210d3f.png',
+        site: 'http://leidos.com',
+        level: 'bronze'
+    },
+    {
+        name: 'Livingston Federal',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/2c13024715bf406ab059cdd95d6a3fd4.jpg',
+        site: 'http://www.livingstonfederal.com/',
+        level: 'silver'
+    },
+    {
+        name: 'Long Term Care Partners',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/11f94a623c9e4ed992a58e0c8fa951e1.jpg',
+        site: 'http://www.ltcpartners.com/',
+        level: 'bronze'
+    },
+    {
+        name: 'Management Concepts',
+        img: 'http://contentz.mkt9052.com/lp/15784/35601/images/mc-logo-top.png',
+        site: 'https://www.managementconcepts.com/',
+        level: 'bronze'
+    },
+    {
+        name: 'Memotec / Comtech EFD',
+        img: 'http://memotec.com/wp-content/themes/memotec/images/logo.png',
+        site: 'http://memotec.com/',
+        level: 'bronze'
+    },
+    {
+        name: 'MITRE Corporation',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/7d4dcdce3fd341969962b57e9bfb9a46.jpg',
+        site: 'http://www.mitre.org/',
+        level: 'exhibitor'
+    },
+    {
+        name: 'Nav Canada ATM',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/dd57d4e9e8ce49a8a1eb88e76b351e11.jpg',
+        site: 'http://www.navcanatm.ca/',
+        level: 'exhibitor'
+    },      
+    {
+        name: 'NBP Corp',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/728199f67bcb480e90c67e0f107f362f.png',
+        site: 'http://www.nbpcorp.com/',
+        level: 'bronze'
+    },
+    {
+        name: 'Plantronics',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/595001422c344c4b98a2ba8430c20c87.png',
+        site: 'http://www.plantronics.com/us/solutions/public-sector/', 
+        level: 'exhibitor'
+    },
+    {
+        name: 'Professional Women Controllers Inc',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/56585e342ec14236845af3026eac98ca.jpg',
+        site: 'http://www.pwcinc.org/',
+        level: 'exhibitor'
+    },
+    {
+        name: 'Raytheon',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/c26ff2b8a1324b11a7a0721b806dca3c.png',
+        site: 'https://www.raytheon.com/',
+        level: 'silver'
+    },
+    {
+        name: 'Regulus',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/37897f2029354e9b8432cecd4d6894af.png',
+        site: 'http://www.regulus-group.com/',
+        level: 'bronze'
+    },
+    {
+        name: 'Rigil',
+        img: 'http://www.rigil.com/sites/default/files//rigil_logo_0_0.jpg',
+        site: 'http://www.rigil.com/',
+        level: 'exhibitor'
+    },
+    {
+        name: 'RussBasset',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/8090ffcb79944e4aa6a06ecce36c5e86.jpg',
+        site: 'https://www.russbassettconsoles.com/airtrafficcontrol',
+        level: 'bronze'
+    },
+    {
+        name: 'SAAB',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/43757d9861364262b427bb717c15712c.png',
+        site: 'http://saab.com/saab-sensis/',
+        level: 'bronze'
+    },
+    {
+        name: 'SAIC',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/992e1558ea884e8b986f29de4db4d84d.jpg',
+        site: 'http://www.saic.com/',
+        level: 'silver'
+    },
+    {
+        name: 'sennheiser',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/a23b920bd14d4f8eb48c5b2f390a9ebc.png',
+        site: 'https://en-us.sennheiser.com/',
+        level: 'exhibitor'
+    },
+    {
+        name: 'skyone',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/fccd18c135684559aef363f57a542b8f.gif',
+        site: 'https://www.skyone.org/',
+        level: 'exhibitor'
+    },
+    {
+        name: 'skysoft',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/e4cc2198d2a74a56b299c8725259a9cb.jpg',
+        site: 'http://www.skysoft-atm.com/',
+        level: 'silver'
+    },
+    {
+        name: 'Snow Federal',
+        img: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=Snow%20Federal&w=200&h=200&bg=FFF',
+        site: 'http://www.snowseminars.com/',
+        level: 'exhibitor'
+    },
+    {
+        name: 'Star Wright USA',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/0f9b5c9c59d94ffd85363fa75faa5056.png',
+        site: 'https://secure.fepla.com/fermapliapp.aspx?1=a50b95d0-5b7e-4f07-b4a6-3d08257bef84',
+        level: 'platinum'
+    },
+    {
+        name: 'Sunhillo',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/1315b9d547854327b01174a7485381b3.jpg',
+        site: 'http://www.sunhillo.com/',
+        level: 'bronze'
+    },
+    {
+        name: 'STR Speechtech LTD',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/ca3b34a0956e4e22bf5e165e4d358eef.jpg',
+        site: 'http://www.speechtech.com/',
+        level: 'exhibitor'
+    },
+    {
+        name: 'systems atlanta',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/2ea75770191443c3b34f7e38f7c69524.png',
+        site: 'http://www.sysatl.com/',
+        level: 'bronze'
+    },
+    {
+        name: 'Thales',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/9ad6aecf8ad74d0e94b3c9792eae5059.gif',
+        site: 'https://www.thalesgroup.com/en',
+        level: 'exhibitor'
+    },
+    {
+        name: 'UFA',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/1b18aa1861da4e26a1d2f967e6a0cbe8.png',
+        site: 'http://ufainc.com',
+        level: 'exhibitor'
+    },
+    {
+        name: 'WAEPA',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/297b17b4737d4b24ba23bfe52b729472.png',
+        site: 'https://www.waepa.org/?utm_source=faama&utm_medium=sponsor&utm_campaign=faama%20expo',
+        level: 'exhibitor'
+    },
+    {
+        name: 'WEYTEC',
+        img: 'https://www.weytec.com/fileadmin/_processed_/a/b/csm_WEYTEC_Logo_RGB_823699313a.jpg',
+        site: 'https://www.weytec.com/',
+        level: 'silver'
+    }
+];
