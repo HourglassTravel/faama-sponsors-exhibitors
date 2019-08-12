7d9578eca9070a8333a6522e77e4b37cc230619b
carterjaycox <45835393+carterjaycox@users.noreply.github.com>
2019-08-12 13:11:03 -0500

    Update exhibitors.js

--- exhibitors.js
+++ exhibitors.js
@@ -31,7 +31,7 @@ const EXHIBITOR_SPONSOR_LIST = [
     },
     {
         name: 'Eizo',
-        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/71d3c0efb5ca404585964b3c026d6493.jpg',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/e58f7095a29b4229a24f50520990a7db.png',
         site: 'http://www.eizo.com/home/',
         level: 'bronze',
     },
