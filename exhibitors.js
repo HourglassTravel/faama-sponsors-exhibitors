d69dc5362adeabfa0ada9d50349862fee3c55e9c
carterjaycox <45835393+carterjaycox@users.noreply.github.com>
2019-08-12 12:48:26 -0500

    Update exhibitors.js

--- exhibitors.js
+++ exhibitors.js
@@ -1,11 +1,5 @@
 const EXHIBITOR_SPONSOR_LIST = [
     {
-        name: 'All Weather Inc',
-        img: 'http://www.atc-network.com/Upload/Industry/33698/AWIlogo200.jpg',
-        site: 'http://www.allweatherinc.com/',
-        level: 'exhibitor'
-    },
-    {
         name: 'American Benefits Exchange',
         img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/7368147004b546f6b0941202f8c862f6.jpg',
         site: 'http://thinkabx.com/',
@@ -30,45 +24,9 @@ const EXHIBITOR_SPONSOR_LIST = [
         level: 'gold'
     },
     {
-        name: 'cobec',
-        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/273d823bbcb6456cb9b65b23f9578f69.png',
-        site: 'http://cobec.com/',
-        level: 'exhibitor'
-    },
-    {
-        name: 'Comtech EFD Memotec',
-        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/cf8aa17f24c241d587f19b6babbf09d0.jpg',
-        site: 'http://www.comtechefdata.com',
-        level: 'exhibitor'
-    },
-    {
-        name: 'cavansolutions',
-        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/3f5e56b69baa4ef6bb9a6407425977b9.png',
-        site: 'http://cavansolutions.com/',
-        level: 'exhibitor'
-    },
-    {
-        name: 'CGH Technologies',
-        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/3a845916717643cba8d88f864b562e9e.jpg',
-        site: 'http://www.cghtech.com/',
-        level: 'exhibitor'
-    },
-    {
         name: 'Concept Seating',
         img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/d4c7679139f849a1a7b9a33401022d1d.jpg',
         site: 'http://www.conceptseating.com/',
-        level: 'exhibitor'
-    },
-    {
-        name: 'Crown Consulting',
-        img: 'http://cignus.aero/wp-content/uploads/2017/02/CCI_Shadow.png',
-        site: 'https://www.crownci.com/',
-        level: 'bronze'
-    },
-    {
-        name: 'EASAT',
-        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/3f826413ec8f49fea4edb8eef93c5c0f.png',
-        site: 'http://www.easat.com/',
         level: 'exhibitor'
     },
     {
@@ -76,12 +34,6 @@ const EXHIBITOR_SPONSOR_LIST = [
         img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/71d3c0efb5ca404585964b3c026d6493.jpg',
         site: 'http://www.eizo.com/home/',
         level: 'bronze',
-    },
-    {
-        name: 'Esterline Federal',
-        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/0086041b6e6347b0a783b07938ae0ad5.png',
-        site: 'https://www.esterline.com/',
-        level: 'exhibitor'
     },
     {
         name: 'Evans Consoles',
@@ -102,6 +54,12 @@ const EXHIBITOR_SPONSOR_LIST = [
         level: 'exhibitor'
     },
     {
+        name: 'Federal Benifits Advisory Group',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/fb96da7d83e64fc7b76227f15b6fc22c.jpg',
+        site: 'https://federalbenefitsadvice.com/',
+        level: 'Silver'
+    },
+    {
         name: 'FEDS',
         img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/1fa501110efb48f486a6c19ddc38de0f.jpg',
         site: 'http://www.fedsprotection.com/',
@@ -114,16 +72,10 @@ const EXHIBITOR_SPONSOR_LIST = [
         level: 'bronze'
     },
     {
-        name: 'First Command',
-        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/129b11a49927453e9d8d88bf09de0135.png',
-        site: 'http://www.firstcommand.com/',
-        level: 'exhibitor'
-    },
-    {
         name: 'Geico',
         img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/0fc79b3dce1343d78a96b832fc330296.jpg',
         site: 'https://www.geico.com/landingpage/member-discount/?logo=70411',
-        level: 'platinum'
+        level: 'silver'
     },
     {
         name: 'General Dynamics',
@@ -135,7 +87,7 @@ const EXHIBITOR_SPONSOR_LIST = [
         name: 'GPIS Employee Benefit Specialist',
         img: 'https://gpis4u.org/wp-content/uploads/2018/06/GPISLogo.png',//??
         site: 'https://gpis4u.org/',
-        level: 'exhibitor'
+        level: 'bronze'
     },
     {
         name: 'Hourglass Travel',
@@ -144,7 +96,13 @@ const EXHIBITOR_SPONSOR_LIST = [
         level: 'bronze'
     },
     {
-        name: 'ISSEusa, LLC',
+        name: 'Huddle',
+        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/0e53f935d6b244caa18d213118299f68.png',
+        site: 'https://www.huddle.com/about/',
+        level: 'bronze'
+    },
+    {
+        name: 'IHSEusa, LLC',
         img: 'http://www.ihseusa.com/images/logo.png',
         site: 'http://www.ihseusa.com/',
         level: 'exhibitor'
@@ -154,24 +112,6 @@ const EXHIBITOR_SPONSOR_LIST = [
         img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/6038e007908b47618dce40010f7e4455.png',
         site: 'http://www.infina.net/',
         level: 'silver'
-    },
-    {
-        name: 'jlansolutions',
-        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/4c32d56fb5674c008b3b7704ab5928c0.png',
-        site: 'http://www.jlansolutions.com/',
-        level: 'exhibitor' 
-    },
-    {
-        name: 'JMA Solutions',
-        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/63dbfe67586e463d901f7ad608f4f00b.jpg',
-        site: 'http://www.jma-solutions.com/',
-        level: 'bronze'
-    },
-    {
-        name: 'kaiser permanente',
-        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/5af24500d9e4437080f34a6191eb0491.jpg',
-        site: 'https://healthplans.kaiserpermanente.org/federalemployees/',
-        level: 'exhibitor'
     },
     {
         name: 'leidos',
@@ -198,7 +138,7 @@ const EXHIBITOR_SPONSOR_LIST = [
         level: 'bronze'
     },
     {
-        name: 'Memotec / Comtech EFD',
+        name: 'Memotec Inc',
         img: 'http://memotec.com/wp-content/themes/memotec/images/logo.png',
         site: 'http://memotec.com/',
         level: 'bronze'
@@ -207,13 +147,7 @@ const EXHIBITOR_SPONSOR_LIST = [
         name: 'MITRE Corporation',
         img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/7d4dcdce3fd341969962b57e9bfb9a46.jpg',
         site: 'http://www.mitre.org/',
-        level: 'exhibitor'
-    },
-    {
-        name: 'Nav Canada ATM',
-        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/dd57d4e9e8ce49a8a1eb88e76b351e11.jpg',
-        site: 'http://www.navcanatm.ca/',
-        level: 'exhibitor'
+        level: 'bronze'
     },      
     {
         name: 'NBP Corp',
@@ -240,27 +174,9 @@ const EXHIBITOR_SPONSOR_LIST = [
         level: 'silver'
     },
     {
-        name: 'Regulus',
-        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/37897f2029354e9b8432cecd4d6894af.png',
-        site: 'http://www.regulus-group.com/',
-        level: 'bronze'
-    },
-    {
-        name: 'Rigil',
-        img: 'http://www.rigil.com/sites/default/files//rigil_logo_0_0.jpg',
-        site: 'http://www.rigil.com/',
-        level: 'exhibitor'
-    },
-    {
         name: 'RussBasset',
         img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/8090ffcb79944e4aa6a06ecce36c5e86.jpg',
         site: 'https://www.russbassettconsoles.com/airtrafficcontrol',
-        level: 'bronze'
-    },
-    {
-        name: 'SAAB',
-        img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/43757d9861364262b427bb717c15712c.png',
-        site: 'http://saab.com/saab-sensis/',
         level: 'bronze'
     },
     {
@@ -279,7 +195,7 @@ const EXHIBITOR_SPONSOR_LIST = [
         name: 'Serco',
         img: 'https://custom.cvent.com/121362D6C5404F6CA3234CDF3C655C82/pix/6dea00bfbe1d48a89e5e7ea93a29d7dd.png',
         site: 'https://www.serco-na.com/',
-        level: 'bronze'
+        level: 'exhibitor'
     },
     {
         name: 'skyone',
@@ -294,7 +210,7 @@ const EXHIBITOR_SPONSOR_LIST = [
         level: 'silver'
     },
     {
-        name: 'Snow Federal',
+        name: 'Snow Federal Retirement Seminars & ChFEBC',
         img: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=Snow%20Federal&w=200&h=200&bg=FFF',
         site: 'http://www.snowseminars.com/',
         level: 'exhibitor'
